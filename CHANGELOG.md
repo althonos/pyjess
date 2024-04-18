37f9750ba5845fa950ea0dc8fdfcd1df0fc48fb1
Martin Larralde <martin.larralde@embl.de>
2024-04-18 13:27:13 +0200

    Release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+## [Unreleased]
+[Unreleased]: https://github.com/althonos/pyjess/compare/v0.1.0...HEAD
+
+
+## [v0.1.0] - 2024-04-18
+[v0.1.0]: https://github.com/althonos/pyjess/compare/3f2a7e9...v0.1.0
+
+Initial release.
--- pyjess/__init__.py
+++ pyjess/__init__.py
@@ -1,6 +1,6 @@
 # noqa: D104
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "Martin Larralde <martin.larralde@embl.de>"
 __all__ = ["Atom", "Hit", "Jess", "Query", "Molecule", "Template", "TemplateAtom"]
 
@@ -8,3 +8,16 @@ from . import _jess
 from ._jess import Atom, Hit, Jess, Query, Molecule, Template, TemplateAtom
 
 __doc__ = _jess.__doc__
+
+# Small addition to the docstring: we want to show a link redirecting to the
+# rendered version of the documentation, but this can only work when Python
+# is running with docstrings enabled
+if __doc__ is not None:
+    __doc__ += """See Also:
+    An online rendered version of the documentation for this version
+    of the library on
+    `Read The Docs <https://pyjess.readthedocs.io/en/v{}/>`_.
+
+    """.format(
+        __version__
+    )
